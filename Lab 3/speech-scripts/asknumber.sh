1bc574d75f71dbfe01ffe88101cd730f9dd8efea
jjjamie <zw1745@nyu.edu>
2023-09-25 18:51:27 -0400

    Update asknumber.sh

--- Lab 3/speech-scripts/asknumber.sh
+++ Lab 3/speech-scripts/asknumber.sh
@@ -1,2 +1,5 @@
-echo 'What's your number?' | \
-  ./piper --model en_US-lessac-medium.onnx --output_file asknumber.wav
+
+# from https://elinux.org/RPi_Text_to_Speech_(Speech_Synthesis)
+espeak -ven+f2 -k5 -s150 --stdout  "What's your number" | aplay
+ 
+python test_microphone.py -m en
