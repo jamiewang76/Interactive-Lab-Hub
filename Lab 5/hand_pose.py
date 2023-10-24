d2edf0e126d62a78b9921fbe3345363dd3fda5f9
jamiewang76 <zw1745@nyu.edu>
2023-10-23 20:12:50 -0400

    Update hand_pose.py

--- Lab 5/hand_pose.py
+++ Lab 5/hand_pose.py
@@ -6,8 +6,21 @@ import math
 from ctypes import cast, POINTER
 import alsaaudio
 m = alsaaudio.Mixer(control='Speaker', cardindex=3)
-m.setvolume(5) 
+m.setvolume(0) 
 import subprocess
+
+import sounddevice as sd
+import numpy as np
+
+# Initialize parameters
+A = 1  # Amplitude
+frequency = 440  # Fixed frequency
+phi = 0  # Phase
+sr = 44100  # Sample rate
+
+# Start the sound stream
+sd_stream = sd.OutputStream(callback=None, channels=1, samplerate=sr, dtype='float32')
+sd_stream.start()
 
 def play_audio():
     command = ['./loop_audio.sh']
@@ -32,6 +45,11 @@ maxVol = 100
 vol = 0
 volBar = 400
 volPer = 0
+
+minFreq = 440
+maxFreq = 1000
+
+
 while True:
     success, img = cap.read()
     img = detector.findHands(img)
@@ -63,21 +81,29 @@ while True:
         length4 = len_calc(thumbX,thumbY, ringX, ringY)
         print(length1,length2,length3)
         condition = length>100 and length1>100 and length2<100 and length3>100 and length4<100
-        if condition:
-            m.setvolume(0)
-            volPer = 0
-            volBar = 400
-            print("CONDITION")
-            cv2.putText(img, 'quiet coyote!', (40, 70), cv2.FONT_HERSHEY_COMPLEX,
-                1, (255, 255, 255), 3)
-        else:
- 
-            vol = np.interp(length, [50, 300], [minVol, maxVol])
-            volBar = np.interp(length, [50, 300], [400, 150])
-            volPer = np.interp(length, [50, 300], [0, 100])
-            m.setvolume(int(vol))
 
-        print(int(length), vol)
+        frequency = np.interp(length, [50, 300], [minFreq, maxFreq])
+        try:
+            t = np.arange(sr) / sr  # Generate a time vector for one second
+            y = A * np.sin(2 * np.pi * frequency * t + phi).astype('float32')
+            sd_stream.write(y)
+        except KeyboardInterrupt:
+            break
+        # if condition:
+        #     m.setvolume(0)
+        #     volPer = 0
+        #     volBar = 400
+        #     print("CONDITION")
+        #     cv2.putText(img, 'quiet coyote!', (40, 70), cv2.FONT_HERSHEY_COMPLEX,
+        #         1, (255, 255, 255), 3)
+        # else:
+ 
+        #     vol = np.interp(length, [50, 300], [minVol, maxVol])
+        #     volBar = np.interp(length, [50, 300], [400, 150])
+        #     volPer = np.interp(length, [50, 300], [0, 100])
+        #     m.setvolume(int(vol))
+
+        # print(int(length), vol)
 
  
         if length < 50:
@@ -99,6 +125,10 @@ while True:
     if cv2.waitKey(1) & 0xFF == ord('q'):  # Press 'q' to quit
         audio_process.terminate()  #
         break
+
+# Stop and close the sound stream
+sd_stream.stop()
+sd_stream.close()
 
 cap.release()
 cv2.destroyAllWindows()
