0d260f03a29a9f039b15ad4b9ec2f9c118660ad2
jamiewang76 <zw1745@nyu.edu>
2023-11-05 18:49:45 -0500

    Create pedestrian_phone.py

---
+++ Lab 5/pedestrian_phone.py
@@ -0,0 +1,69 @@
+import cv2
+import mediapipe as mp
+import numpy as np
+from mediapipe import solutions
+from mediapipe.framework.formats import landmark_pb2
+from mediapipe.tasks import python
+
+BaseOptions = mp.tasks.BaseOptions
+FaceLandmarker = mp.tasks.vision.FaceLandmarker
+FaceLandmarkerOptions = mp.tasks.vision.FaceLandmarkerOptions
+FaceLandmarkerResult = mp.tasks.vision.FaceLandmarkerResult
+VisionRunningMode = mp.tasks.vision.RunningMode
+
+
+def print_result(result: FaceLandmarkerResult, output_image: mp.Image, timestamp_ms: int):
+    print('face landmarker result: {}'.format(result))
+    annotated_image = draw_landmarks_on_image(imgRGB, result)
+    image_result = cv2.cvtColor(annotated_image, cv2.COLOR_RGB2BGR)
+    cv2.imshow("frame", image_result)
+    cv2.waitKey(1)
+
+
+def draw_landmarks_on_image(rgb_image, detection_result):
+    face_landmarks_list = detection_result.face_landmarks
+    annotated_image = np.copy(rgb_image)
+    for idx in range(len(face_landmarks_list)):
+        face_landmarks = face_landmarks_list[idx]
+        face_landmarks_proto = landmark_pb2.NormalizedLandmarkList()
+        face_landmarks_proto.landmark.extend([
+          landmark_pb2.NormalizedLandmark(x=landmark.x, y=landmark.y, z=landmark.z) for landmark in face_landmarks
+        ])
+        solutions.drawing_utils.draw_landmarks(
+            image=annotated_image,
+            landmark_list=face_landmarks_proto,
+            connections=mp.solutions.face_mesh.FACEMESH_TESSELATION,
+            landmark_drawing_spec=None,
+            connection_drawing_spec=mp.solutions.drawing_styles
+            .get_default_face_mesh_tesselation_style())
+        solutions.drawing_utils.draw_landmarks(
+            image=annotated_image,
+            landmark_list=face_landmarks_proto,
+            connections=mp.solutions.face_mesh.FACEMESH_CONTOURS,
+            landmark_drawing_spec=None,
+            connection_drawing_spec=mp.solutions.drawing_styles
+            .get_default_face_mesh_contours_style())
+        solutions.drawing_utils.draw_landmarks(
+            image=annotated_image,
+            landmark_list=face_landmarks_proto,
+            connections=mp.solutions.face_mesh.FACEMESH_IRISES,
+              landmark_drawing_spec=None,
+              connection_drawing_spec=mp.solutions.drawing_styles
+              .get_default_face_mesh_iris_connections_style())
+    return annotated_image
+
+
+options = FaceLandmarkerOptions(
+    base_options=BaseOptions(model_asset_path='face_landmarker_v2_with_blendshapes.task'),
+    running_mode=VisionRunningMode.LIVE_STREAM,
+    result_callback=print_result)
+i = 0
+with FaceLandmarker.create_from_options(options) as landmarker:
+    cap = cv2.VideoCapture(0)
+    while True:
+        succes, img = cap.read()
+        i += 1
+        imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        mp_image = mp.Image(image_format=mp.ImageFormat.SRGB, data=imgRGB)
+        landmarker.detect_async(mp_image, i)
+        print(i)
