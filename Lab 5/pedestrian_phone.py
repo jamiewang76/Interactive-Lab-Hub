b465841cb73f653297c33dfb40750a25dbc80334
jamiewang76 <zw1745@nyu.edu>
2023-11-05 19:18:14 -0500

    Update pedestrian_phone.py

--- Lab 5/pedestrian_phone.py
+++ Lab 5/pedestrian_phone.py
@@ -41,20 +41,20 @@ def print_result(result: PoseLandmarkerResult, output_image: mp.Image, timestamp
 #   return annotated_image
 
 def draw_landmarks_on_image(rgb_image, detection_result):
-    pose_landmarks_list = detection_result.pose_landmarks
+    # pose_landmarks_list = detection_result.pose_landmarks
     annotated_image = np.copy(rgb_image)
-    for idx in range(len(pose_landmarks_list)):
-        pose_landmarks = pose_landmarks_list[idx]
-        pose_landmarks_proto = landmark_pb2.NormalizedLandmarkList()
-        pose_landmarks_proto.landmark.extend([
-           landmark_pb2.NormalizedLandmark(x=landmark.x, y=landmark.y, z=landmark.z) for landmark in pose_landmarks
-    ])
-        solutions.drawing_utils.draw_landmarks(
-            image=annotated_image,
-            landmark_list=pose_landmarks_proto,
-            connections=solutions.pose.POSE_CONNECTIONS,
-            landmark_drawing_spec=None,
-            connection_drawing_spec=solutions.drawing_styles.get_default_pose_landmarks_style())
+    # for idx in range(len(pose_landmarks_list)):
+    #     pose_landmarks = pose_landmarks_list[idx]
+    #     pose_landmarks_proto = landmark_pb2.NormalizedLandmarkList()
+    #     pose_landmarks_proto.landmark.extend([
+    #        landmark_pb2.NormalizedLandmark(x=landmark.x, y=landmark.y, z=landmark.z) for landmark in pose_landmarks
+    # ])
+    #     solutions.drawing_utils.draw_landmarks(
+    #         image=annotated_image,
+    #         landmark_list=pose_landmarks_proto,
+    #         connections=solutions.pose.POSE_CONNECTIONS,
+    #         landmark_drawing_spec=None,
+    #         connection_drawing_spec=solutions.drawing_styles.get_default_pose_landmarks_style())
     return annotated_image
 
 
