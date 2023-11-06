be8580870f671cad538dd469f4125c919b143d0a
jamiewang76 <zw1745@nyu.edu>
2023-11-05 19:15:34 -0500

    Update pedestrian_phone.py

--- Lab 5/pedestrian_phone.py
+++ Lab 5/pedestrian_phone.py
@@ -20,57 +20,42 @@ def print_result(result: PoseLandmarkerResult, output_image: mp.Image, timestamp
     cv2.imshow("frame", image_result)
     cv2.waitKey(1)
 
-def draw_landmarks_on_image(rgb_image, detection_result):
-  pose_landmarks_list = detection_result.pose_landmarks
-  annotated_image = np.copy(rgb_image)
-
-  # Loop through the detected poses to visualize.
-  for idx in range(len(pose_landmarks_list)):
-    pose_landmarks = pose_landmarks_list[idx]
-
-    # Draw the pose landmarks.
-    pose_landmarks_proto = landmark_pb2.NormalizedLandmarkList()
-    pose_landmarks_proto.landmark.extend([
-      landmark_pb2.NormalizedLandmark(x=landmark.x, y=landmark.y, z=landmark.z) for landmark in pose_landmarks
-    ])
-    solutions.drawing_utils.draw_landmarks(
-      annotated_image,
-      pose_landmarks_proto,
-      solutions.pose.POSE_CONNECTIONS,
-      solutions.drawing_styles.get_default_pose_landmarks_style())
-  return annotated_image
-
 # def draw_landmarks_on_image(rgb_image, detection_result):
-#     face_landmarks_list = detection_result.face_landmarks
-#     annotated_image = np.copy(rgb_image)
-#     for idx in range(len(face_landmarks_list)):
-#         face_landmarks = face_landmarks_list[idx]
-#         face_landmarks_proto = landmark_pb2.NormalizedLandmarkList()
-#         face_landmarks_proto.landmark.extend([
-#           landmark_pb2.NormalizedLandmark(x=landmark.x, y=landmark.y, z=landmark.z) for landmark in face_landmarks
-#         ])
-#         solutions.drawing_utils.draw_landmarks(
-#             image=annotated_image,
-#             landmark_list=face_landmarks_proto,
-#             connections=mp.solutions.face_mesh.FACEMESH_TESSELATION,
-#             landmark_drawing_spec=None,
-#             connection_drawing_spec=mp.solutions.drawing_styles
-#             .get_default_face_mesh_tesselation_style())
-#         solutions.drawing_utils.draw_landmarks(
-#             image=annotated_image,
-#             landmark_list=face_landmarks_proto,
-#             connections=mp.solutions.face_mesh.FACEMESH_CONTOURS,
-#             landmark_drawing_spec=None,
-#             connection_drawing_spec=mp.solutions.drawing_styles
-#             .get_default_face_mesh_contours_style())
-#         solutions.drawing_utils.draw_landmarks(
-#             image=annotated_image,
-#             landmark_list=face_landmarks_proto,
-#             connections=mp.solutions.face_mesh.FACEMESH_IRISES,
-#               landmark_drawing_spec=None,
-#               connection_drawing_spec=mp.solutions.drawing_styles
-#               .get_default_face_mesh_iris_connections_style())
-#     return annotated_image
+#   pose_landmarks_list = detection_result.pose_landmarks
+#   annotated_image = np.copy(rgb_image)
+
+#   # Loop through the detected poses to visualize.
+#   for idx in range(len(pose_landmarks_list)):
+#     pose_landmarks = pose_landmarks_list[idx]
+
+#     # Draw the pose landmarks.
+#     pose_landmarks_proto = landmark_pb2.NormalizedLandmarkList()
+#     pose_landmarks_proto.landmark.extend([
+#       landmark_pb2.NormalizedLandmark(x=landmark.x, y=landmark.y, z=landmark.z) for landmark in pose_landmarks
+#     ])
+#     solutions.drawing_utils.draw_landmarks(
+#       annotated_image,
+#       pose_landmarks_proto,
+#       solutions.pose.POSE_CONNECTIONS,
+#       solutions.drawing_styles.get_default_pose_landmarks_style())
+#   return annotated_image
+
+def draw_landmarks_on_image(rgb_image, detection_result):
+    pose_landmarks_list = detection_result.pose_landmarks
+    annotated_image = np.copy(rgb_image)
+    for idx in range(len(pose_landmarks_list)):
+        pose_landmarks = pose_landmarks_list[idx]
+        pose_landmarks_proto = landmark_pb2.NormalizedLandmarkList()
+        pose_landmarks_proto.landmark.extend([
+           landmark_pb2.NormalizedLandmark(x=landmark.x, y=landmark.y, z=landmark.z) for landmark in pose_landmarks
+    ])
+        solutions.drawing_utils.draw_landmarks(
+            image=annotated_image,
+            landmark_list=pose_landmarks_proto,
+            connections=solutions.pose.POSE_CONNECTIONS,
+            landmark_drawing_spec=None,
+            connection_drawing_spec=solutions.drawing_styles.get_default_pose_landmarks_style())
+    return annotated_image
 
 
 options = PoseLandmarkerOptions(
