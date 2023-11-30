4580bd8f50088a440e19e471f8fb4861b5a8923e
jamiewang76 <zw1745@nyu.edu>
2023-11-29 22:31:29 -0500

    Update clicksend.py

--- Well Done/clicksend.py
+++ Well Done/clicksend.py
@@ -3,7 +3,7 @@ username = 'ziyingwang76@gmail.com' # Your ClickSend username
 api_key = '0592FD37-2D0D-150E-7A56-000E6821998B' # Your Secure Unique API key 
 msg_to = '+19178259760' # Recipient Mobile Number in international format (+61411111111 test number). 
 msg_from = '' # Custom sender ID (leave blank to accept replies). 
-msg_body = 'This is a test message' # The message to be sent. 
+msg_body = 'This message is from PI' # The message to be sent. 
 import json, subprocess 
 request = { "messages" : [ { "source":"rpi", "from":msg_from, "to":msg_to, "body":msg_body } ] } 
 request = json.dumps(request) 
