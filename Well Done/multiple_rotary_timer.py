e256a7dddabbb4d58709d3fd3b482f657e30034a
jamiewang76 <zw1745@nyu.edu>
2023-11-30 01:15:07 -0500

    Update multiple_rotary_timer.py

--- Well Done/multiple_rotary_timer.py
+++ Well Done/multiple_rotary_timer.py
@@ -3,7 +3,12 @@
 
 # I2C rotary encoder multiple test example.
 # solder the A0 jumper on the second QT Rotary Encoder board
-
+username = 'ziyingwang76@gmail.com' # Your ClickSend username 
+api_key = '0592FD37-2D0D-150E-7A56-000E6821998B' # Your Secure Unique API key 
+msg_to = '+19178259760' # Recipient Mobile Number in international format (+61411111111 test number). 
+msg_from = '' # Custom sender ID (leave blank to accept replies). 
+msg_body = 'This message is from PI' # The message to be sent. 
+import json, subprocess 
 import board
 import time
 import threading
@@ -15,12 +20,12 @@ i2c = board.I2C()  # uses board.SCL and board.SDA
 
 # clicksend
 # -*- coding: utf-8 -*- 
-username = 'ziyingwang76@gmail.com' # Your ClickSend username 
-api_key = '0592FD37-2D0D-150E-7A56-000E6821998B' # Your Secure Unique API key 
-msg_to = '+19178259760' # Recipient Mobile Number in international format (+61411111111 test number). 
-msg_from = '' # Custom sender ID (leave blank to accept replies). 
-msg_body = 'This message is from PI' # The message to be sent. 
-import json, subprocess 
+# username = 'ziyingwang76@gmail.com' # Your ClickSend username 
+# api_key = '0592FD37-2D0D-150E-7A56-000E6821998B' # Your Secure Unique API key 
+# msg_to = '+19178259760' # Recipient Mobile Number in international format (+61411111111 test number). 
+# msg_from = '' # Custom sender ID (leave blank to accept replies). 
+# msg_body = 'This message is from PI' # The message to be sent. 
+# import json, subprocess 
 # request = { "messages" : [ { "source":"rpi", "from":msg_from, "to":msg_to, "body":msg_body } ] } 
 # request = json.dumps(request) 
 # cmd = "curl https://rest.clicksend.com/v3/sms/send -u " + username + ":" + api_key + " -H \"Content-Type: application/json\" -X POST --data-raw '" + request + "'" 
