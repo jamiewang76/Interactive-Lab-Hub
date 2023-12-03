39cf4dcf67175f5382b2461cbbb6f9e4e2295bfe
jamiewang76 <zw1745@nyu.edu>
2023-12-02 23:35:41 -0500

    Update multiple_rotary_timer.py

--- Well Done/multiple_rotary_timer.py
+++ Well Done/multiple_rotary_timer.py
@@ -257,16 +257,16 @@ while True:
 
     if time_left1 != 0:
         draw.text((0.65*width, 0.1*height), str(time_left1), font=font, fill=(255, 255, 255))
-        draw.text((0.75*width, 0.1*height), " left", font=font, fill=(255, 255, 255))
+        draw.text((0.8*width, 0.1*height), " left", font=font, fill=(255, 255, 255))
     if time_left2 != 0:
         draw.text((0.65*width, 0.3*height), str(time_left2), font=font, fill=(255, 255, 255))
-        draw.text((0.75*width, 0.3*height), " left", font=font, fill=(255, 255, 255))
+        draw.text((0.8*width, 0.3*height), " left", font=font, fill=(255, 255, 255))
     if time_left3 != 0:
         draw.text((0.65*width, 0.5*height), str(time_left3), font=font, fill=(255, 255, 255))
-        draw.text((0.75*width, 0.5*height), " left", font=font, fill=(255, 255, 255))
+        draw.text((0.8*width, 0.5*height), " left", font=font, fill=(255, 255, 255))
     if time_left4 != 0:
         draw.text((0.65*width, 0.7*height), str(time_left4), font=font, fill=(255, 255, 255))
-        draw.text((0.75*width, 0.7*height), " left", font=font, fill=(255, 255, 255))
+        draw.text((0.8*width, 0.7*height), " left", font=font, fill=(255, 255, 255))
     disp.image(image, rotation)
 
 
