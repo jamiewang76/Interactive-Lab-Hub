0ad0a91a91000f1afbbacc3251a16010e59a25de
jamiewang76 <zw1745@nyu.edu>
2023-12-05 21:19:00 -0500

    Update multiple_rotary_timer.py

--- Well Done/multiple_rotary_timer.py
+++ Well Done/multiple_rotary_timer.py
@@ -296,7 +296,7 @@ while True:
             draw.text((0.85*width, 0.1*height), "", font=font, fill=(255, 255, 255))
     if time_left1 == 0:
         if timer1_started:
-            draw.text((0.7*width, 0.1*height), "Done!", font=font, fill=(255, 255, 255))
+            draw.text((0.7*width, 0.1*height), "Done!", font=font, fill=(27, 255, 155))
     if time_left2 != 0:
         if timer2_started:
             draw.text((0.7*width, 0.3*height), str(time_left2), font=font, fill=(255, 255, 255))
@@ -305,7 +305,7 @@ while True:
             draw.text((0.85*width, 0.3*height), "", font=font, fill=(255, 255, 255))
     if time_left2 == 0:
         if timer2_started:
-            draw.text((0.7*width, 0.3*height), "Done!", font=font, fill=(255, 255, 255))
+            draw.text((0.7*width, 0.3*height), "Done!", font=font, fill=(27, 255, 155))
     if time_left3 != 0:
         if timer3_started:
             draw.text((0.7*width, 0.5*height), str(time_left3), font=font, fill=(255, 255, 255))
@@ -314,7 +314,7 @@ while True:
             draw.text((0.85*width, 0.5*height), "", font=font, fill=(255, 255, 255))
     if time_left3 == 0:
         if timer3_started:
-            draw.text((0.7*width, 0.5*height), "Done!", font=font, fill=(255, 255, 255))
+            draw.text((0.7*width, 0.5*height), "Done!", font=font, fill=(27, 255, 155))
     if time_left4 != 0:
         if timer4_started:
             draw.text((0.7*width, 0.7*height), str(time_left4), font=font, fill=(255, 255, 255))
@@ -323,7 +323,7 @@ while True:
             draw.text((0.85*width, 0.7*height), "", font=font, fill=(255, 255, 255))
     if time_left4 == 0:
         if timer4_started:
-            draw.text((0.7*width, 0.7*height), "Done!", font=font, fill=(255, 255, 255))
+            draw.text((0.7*width, 0.7*height), "Done!", font=font, fill=(27, 255, 155))
 
 
 
